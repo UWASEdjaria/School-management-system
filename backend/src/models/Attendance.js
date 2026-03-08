2c1a1490560bccd5c1e18d5d67890cf706a542a5
Djaria UWASE <uwasedjaria41@gmail.com>
2026-03-08 16:59:48 +0200

    project set-up

--- backend/src/models/Attendance.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const attendanceSchema = new mongoose.Schema({
-  studentId: { type: mongoose.Schema.Types.ObjectId, ref: 'Student', required: true },
-  date: { type: Date, required: true },
-  status: { type: String, enum: ['present', 'absent', 'late'], required: true }
-}, { timestamps: true });
-
-export default mongoose.model('Attendance', attendanceSchema);
--- backend/src/utils/hash.js
+++
@@ -1,9 +0,0 @@
-import crypto from 'crypto';
-
-export const hashPassword = (password) => {
-  return crypto.createHash('sha512').update(password).digest('hex');
-};
-
-export const comparePassword = (password, hash) => {
-  return hashPassword(password) === hash;
-};
